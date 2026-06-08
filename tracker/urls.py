bd0c52430134b1d7dea2f1fe5810b39f3b780f26
arthurwamala1 <arthurwamala1@gmail.com>
2026-06-08 22:31:47 +0300

    Fixed routes

--- tracker/urls.py
+++ tracker/urls.py
@@ -3,16 +3,15 @@ from . import views
 
 urlpatterns = [
 
-path('',views.dashboard,name='dashboard'),
+    path('', views.dashboard, name='dashboard'),
 
-path(
-'add-bus/',
-views.add_bus
-),
+    path('add-bus/', views.add_bus, name='add_bus'),
+    path('subtract-bus/', views.subtract_bus, name='subtract_bus'),
 
-path(
-'subtract-bus/',
-views.subtract_bus
-),
+    path('add-coster/', views.add_coster, name='add_coster'),
+    path('subtract-coster/', views.subtract_coster, name='subtract_coster'),
+
+    path('add-taxi/', views.add_taxi, name='add_taxi'),
+    path('subtract-taxi/', views.subtract_taxi, name='subtract_taxi'),
 
 ]
