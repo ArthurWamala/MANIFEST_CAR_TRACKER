0005388a2f3d424b8f4dbdfb2977300c6b1617f9
arthurwamala1 <arthurwamala1@gmail.com>
2026-06-09 18:38:29 +0300

    final version

--- reports/views.py
+++ reports/views.py
@@ -1,15 +1,40 @@
 from django.shortcuts import render
 from tracker.models import VehicleCount
+from events.models import Event
 
+def reports_dashboard(request):
 
-def report(request):
+    event = Event.objects.filter(
+        active=True
+    ).first()
 
-    records = VehicleCount.objects.all()
+    records = VehicleCount.objects.filter(
+        event=event
+    )
+
+    total_bus = sum(
+        x.count for x in records
+        if x.vehicle_type == 'BUS'
+    )
+
+    total_coster = sum(
+        x.count for x in records
+        if x.vehicle_type == 'COSTER'
+    )
+
+    total_taxi = sum(
+        x.count for x in records
+        if x.vehicle_type == 'TAXI'
+    )
 
     return render(
         request,
         'reports/report.html',
         {
-            'records':records
+            'event':event,
+            'records':records,
+            'total_bus':total_bus,
+            'total_coster':total_coster,
+            'total_taxi':total_taxi
         }
     )
